0e21232aa68df97aadb90b19d1f75040c633db68
Manish Kumar <manish.kumar@commitbit.com>
2025-12-08 06:05:22 -0600

    fixed

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
     await connectDB();
     console.log("✅ Database connected successfully");
 
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 10000;
     app.listen(PORT, () => {
       console.log(`🚀 Server running in ${process.env.NODE_ENV || "development"} mode on port ${PORT}`);
     });
